e4539b37168631baec7dee949c44d42604fbcbed
Aishwarya Mensinkai <aishwaryamensinkai@gmail.com>
2024-03-07 00:31:34 -0500

    config files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import UilReact from "@iconscout/react-unicons/icons/uil-react";
+import "tailwindcss/tailwind.css";
 
 function App() {
   return (
-    <div className="App">
-      <UilReact size="140" color="#61DAFB" />
+    <div
+      className="mx-auto max-w-screen-md mt-4 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 
+    h-fit shadow-xl shadow-gray-400 "
+    >
+      hi
     </div>
   );
 }
