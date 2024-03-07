f3211a881919d003f365571390ba8e3d6713cfe4
Aishwarya Mensinkai <aishwaryamensinkai@gmail.com>
2024-03-06 23:50:19 -0500

    added tailwind and react-unicons packages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import './App.css';
+import "./App.css";
+import UilReact from "@iconscout/react-unicons/icons/uil-react";
 
 function App() {
   return (
     <div className="App">
+      <UilReact size="140" color="#61DAFB" />
     </div>
   );
 }
